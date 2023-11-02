7dbfdf858c68a0e387cdd4f3603b2018a16f9a65
SUHO PARK <duglikespiano@gmail.com>
2023-11-02 22:49:11 +0900

    DAY5 - BLURRY LOADING

---
+++ 5_blurry_loading/script.js
@@ -0,0 +1,22 @@
+const loadingText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 10);
+
+function blurring() {
+	load++;
+	if (load > 99) {
+		clearInterval(int);
+	}
+	loadingText.innerText = `${load}%`;
+	loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+	bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+	return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
