c4b6ed100b67cf4eea534b2c18fcaeb4223b222e
SUHO PARK <duglikespiano@gmail.com>
2023-11-23 01:43:06 +0900

    DAY35 - IMAGE CAROUSEL

---
+++ 35_image_carousel/script.js
@@ -0,0 +1,40 @@
+const images = document.getElementById('images');
+const leftButton = document.getElementById('left');
+const rightButton = document.getElementById('right');
+
+const image = document.querySelectorAll('img');
+
+let index = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+	index++;
+	changeImage();
+}
+
+function changeImage() {
+	if (index > image.length - 1) {
+		index = 0;
+	} else if (index < 0) {
+		index = image.length - 1;
+	}
+	images.style.transform = `translateX(${-index * 500}px)`;
+}
+
+function resetInterval() {
+	clearInterval(interval);
+	interval = setInterval(run, 2000);
+}
+
+rightButton.addEventListener('click', () => {
+	index++;
+	changeImage();
+	resetInterval();
+});
+
+leftButton.addEventListener('click', () => {
+	index--;
+	changeImage();
+	resetInterval();
+});
